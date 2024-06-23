4c6c5d9b6ff6b2ac844a0a6e425c6c1e26cf9666
Lucas Code <lucascode.ctt@gmail.com>
2024-06-23 12:51:00 -0300

    Add files via upload

---
+++ aula(2.1).c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	printf("valor inteiro: %d \n", 10);
+	printf("valor real: %f \n", 10.23456789);
+	printf("valor real com apenas duas casas: %.2f \n", 10.2345);
+	printf("dado de texto: %s \n", "teste");
+}
