8fb1af749761d08c9db5f95e61d685cbe86256f5
Lucas Code <lucascode.ctt@gmail.com>
2024-06-24 17:09:05 -0300

    Add files via upload

---
+++ aula(3.2).c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("operadores aritméticos:\n\n");
+	
+	int A, B, soma, sub, mult, div;
+	
+	printf("digite o primeiro valor: ");
+	scanf("%d", &A);
+	
+	printf("digite o segundo valor: ");
+	scanf("%d", &B);
+	
+	soma = A + B;
+	sub = A - B;
+	mult = A * B;
+	div = A % B;
+	
+	printf("\nresultados:\n\n");
+	printf("soma dos valores: %d\n", soma);
+	printf("subtração dos valores: %d\n", sub);
+	printf("multiplicação dos valores: %d\n", mult);
+	printf("divisão dos valores: %d\n\n", div);
+	
+	printf("atribuições aritméticas:\n\n");
+	
+	int dado = 10;
+	printf("dado antes do incremento: %d\n", dado);
+	
+	dado++;
+	printf("depois do incremento: %d\n", dado);
+	
+	dado--;
+	printf("depois do decrimento: %d\n", dado);
+	
+	dado += 5;
+	printf("depois do incremento com 5 unidades: %d\n", dado);
+	
+	dado -= 2;
+	printf("depois do decrimento de 2 unidades: %d\n", dado);
+	
+	dado *= 10;
+	printf("depois de multiplicar em 10 unidades: %d\n", dado);
+	
+}
