d2cd350f25936797250a0ce3acc325fac1b42f89
Lucas Code <lucascode.ctt@gmail.com>
2024-07-26 11:35:03 -0300

    Add files via upload

---
+++ aula(8.0.1) - Structs (struct e typedef).c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+#define TAM 50
+
+struct tipo_pessoa{
+	int idade;
+	float peso;
+	char nome[TAM];
+};
+
+typedef struct tipo_pessoa tipo_pessoa;
+
+int main(){
+	setlocale(LC_ALL,"Portuguese");
+
+	//Criando e inicializando
+	tipo_pessoa pes = {0, 0.0, "Teste"};
+	
+	puts("Início:");
+	printf("pes.idade: %d\n", pes.idade);
+	printf("pes.peso: %.2f\n", pes.peso);
+	printf("pes.nome: %s\n", pes.nome);
+	
+	//Atribuindo valores aos campos
+	pes.idade = 10;
+	pes.peso = 99.99;
+	strcpy(pes.nome, "Texto alterado");
+	
+	puts("\nAlterando os campos via código:");
+	printf("pes.idade: %d\n", pes.idade);
+	printf("pes.peso: %.2f\n", pes.peso);
+	printf("pes.nome: %s\n", pes.nome);
+	
+	//Solicitando inserções via usuário
+	
+	puts("\nInsira uma idade:");
+	scanf("%d", &pes.idade);
+	fflush(stdin);
+	
+	puts("Insira um peso:");
+	scanf("%f", &pes.peso);
+	fflush(stdin);
+	
+	puts("Insira um nome:");
+	scanf("%[^\n]s", &pes.nome);
+	fflush(stdin);
+	
+	puts("\nDados inseridos pelo usuário:");
+	printf("pes.idade: %d\n", pes.idade);
+	printf("pes.peso: %.2f\n", pes.peso);
+	printf("pes.nome: %s\n", pes.nome);
+	
+	return 0;
+}
+
---
+++ aula(8.0.2) - Structs (structs + listas).c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#include <stdlib.h>
+
+#define TAM 3
+
+struct tipo_pessoa{
+	int idade;
+	float peso;
+	char nome[50];
+};
+
+typedef struct tipo_pessoa tipo_pessoa;
+
+int main(){
+	setlocale(LC_ALL,"Portuguese");
+
+	tipo_pessoa lista[TAM];
+	int i;
+	
+	for(i=0;i<TAM;i++){
+		printf("Insira os Dados no registro %d:\n\n", i+1);
+		
+		printf("Nome: ");
+		scanf("%50[^\n]s", &lista[i]. nome);
+		fflush(stdin);
+		
+		printf("Idade: ");
+		scanf("%d", &lista[i].idade);
+		fflush(stdin);
+		
+		printf("Peso: ");
+		scanf("%f", &lista[i].peso);
+		fflush(stdin);
+		
+		puts("");
+	};
+	
+	system("cls");
+	system("color 0A");
+	
+	puts("\tRegistro de dados");
+	
+	for(i=0;i<TAM;i++){
+		printf("\n----------- Pessoa %d -----------\n\n", i+1);
+		printf("\tNome: %s\n", lista[i].nome);
+		printf("\tIdade: %d\n", lista[i].idade);
+		printf("\tPeso: %.2f kg\n", lista[i].peso);
+	}
+	printf("\n--------------------------------");
+
+	return 0;
+}
+
